d3ae03913028c4aa5afd26428d31fc4c69419162
Jasik29 <suiebayzh@gmail.com>
2021-01-29 16:01:04 +0600

    Added Fonts and TextStyles

---
+++ lib/core/consts/text_styles.dart
@@ -0,0 +1,95 @@
+import 'package:doover_project_test/core/consts/colors.dart';
+import 'package:flutter/material.dart';
+
+class DooverTextStyles {
+
+  static const TextStyle kAppBarTextStyle = TextStyle(
+    color: DooverColors.kCardBackgroundColor,
+    fontSize: 17,
+    fontFamily: 'Museo500',
+  );
+
+  static const TextStyle kNonPrimaryTextStyle = TextStyle(
+    color: DooverColors.kBottomNavBarConstItemColor,
+    fontSize: 16,
+    fontFamily: 'MuseoRegular',
+  );
+
+  static const TextStyle kCategoryTitleTextStyle = TextStyle(
+    color: DooverColors.kBottomNavBarSelectedItemColor,
+    fontSize: 14,
+    fontFamily: 'MuseoBold',
+  );
+
+  static const TextStyle kDialogueTitleTextStyle = TextStyle(
+    color: DooverColors.kBottomNavBarSelectedItemColor,
+    fontSize: 20,
+    fontFamily: 'MuseoBold',
+  );
+
+  static const TextStyle kNumbersTextStyle = TextStyle(
+    color: DooverColors.kBottomNavBarSelectedItemColor,
+    fontSize: 14,
+    fontFamily: 'MuseoRegular',
+  );
+
+  static const TextStyle kNotificationTextStyle = TextStyle(
+    color: DooverColors.kBottomNavBarSelectedItemColor,
+    fontSize: 16,
+    fontFamily: 'MuseoRegular',
+  );
+
+  static const TextStyle kLogOutTextStyle = TextStyle(
+    color: DooverColors.kCancelColor,
+    fontSize: 16,
+    fontFamily: 'MuseoRegular',
+  );
+
+  static const TextStyle kCancelTextStyle = TextStyle(
+    color: DooverColors.kCancelColor,
+    fontSize: 17,
+    fontFamily: 'MuseoRegular',
+  );
+
+  static const TextStyle kLogInTextStyle = TextStyle(
+    color: DooverColors.kScaffoldBackgroundColor,
+    fontSize: 16,
+    fontFamily: 'Museo500',
+  );
+
+  static const TextStyle kSumTextStyle = TextStyle(
+    color: DooverColors.kBottomNavBarSelectedBackgroundColor,
+    fontSize: 16,
+    fontFamily: 'Museo500',
+  );
+
+  static const TextStyle kDeadlineTextStyle = TextStyle(
+    color: DooverColors.kBottomNavBarConstItemColor,
+    fontSize: 14,
+    fontFamily: 'MuseoRegular',
+  );
+
+  static const TextStyle kCategoryDescriptionTextStyle = TextStyle(
+    color: DooverColors.kBottomNavBarConstItemColor,
+    fontSize: 12,
+    fontFamily: 'MuseoRegular',
+  );
+
+  static const TextStyle kOnSearchTextStyle = TextStyle(
+    color: DooverColors.kBottomNavBarSelectedItemColor,
+    fontSize: 16,
+    fontFamily: 'Museo500',
+  );
+
+  static const TextStyle kNavBarSelectedTextStyle = TextStyle(
+    color: DooverColors.kBottomNavBarSelectedItemColor,
+    fontSize: 10,
+    fontFamily: 'Museo500',
+  );
+
+  static const TextStyle kNavBarConstTextStyle = TextStyle(
+    color: DooverColors.kBottomNavBarConstItemColor,
+    fontSize: 10,
+    fontFamily: 'Museo500',
+  );
+}
