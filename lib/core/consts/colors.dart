8c7d8357f99d83dc622e3227ba8f519941f0e1da
Jasik29 <suiebayzh@gmail.com>
2021-01-29 15:06:07 +0600

    Added const colors.dart

---
+++ lib/core/consts/colors.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+class DooverColors {
+  static const Color kScaffoldBackgroundColor = Color(0xFFE5E5E5);
+
+  static const Color kCardBackgroundColor = Color(0xFFFFFFFF);
+
+  static const Color kBottomNavBarSelectedItemColor = Color(0xFF172853);
+
+  static const Color kBottomNavBarConstItemColor = Color(0xFFB0B3BC);
+
+  static const Color kBottomNavBarSelectedColor = Color(0xFFF6F8FC);
+
+  static const Color kButtonColor = Color(0xFF4376FB);
+
+  static const Color kQuestionBackgroundColor = Color(0xFFEDEFF6);
+}
